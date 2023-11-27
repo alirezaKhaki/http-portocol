2d04c0f5ee6aaa5c208af2655a0bc35fda920b9e
Alireza Mansourkhaki <a.mansourkhaki@domil.io>
2023-11-27 17:59:32 +0330

    add html file

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,9 @@ import path from "path";
 const app = express();
 const port = 3000;
 
+// Use middleware to parse JSON and urlencoded request bodies
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/home", (req: Request, res: Response) => {
@@ -13,14 +16,16 @@ app.get("/home", (req: Request, res: Response) => {
 
 // Define a route to handle GET requests
 app.get("/get", (req: Request, res: Response) => {
-  res.send("This is a GET request.");
+  const data = req.query.data;
+  res.send(`Response from GET request: ${data}`);
 });
 
 // Define a route to handle POST requests
 app.post("/post", (req: Request, res: Response) => {
-  res.send("This is a POST request.");
+  const data = req.body;
+  console.log("Data from POST request:", data);
+  res.json({ response: data });
 });
-
 // Define a route to handle PUT requests
 app.put("/put", (req: Request, res: Response) => {
   res.send("This is a PUT request.");
