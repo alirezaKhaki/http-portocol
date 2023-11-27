e61dc19f63d43aea9a55a8bfdc8c296a7292b433
Alireza Mansourkhaki <a.mansourkhaki@domil.io>
2023-11-27 17:36:10 +0330

    add server

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,33 @@
 import express, { Request, Response } from "express";
+import path from "path";
 
 // Create an Express application
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/home", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
+
 // Define a route to handle GET requests
-app.get("/", (req: Request, res: Response) => {
+app.get("/get", (req: Request, res: Response) => {
   res.send("This is a GET request.");
 });
 
 // Define a route to handle POST requests
-app.post("/", (req: Request, res: Response) => {
+app.post("/post", (req: Request, res: Response) => {
   res.send("This is a POST request.");
 });
 
 // Define a route to handle PUT requests
-app.put("/", (req: Request, res: Response) => {
+app.put("/put", (req: Request, res: Response) => {
   res.send("This is a PUT request.");
 });
 
 // Define a route to handle DELETE requests
-app.delete("/", (req: Request, res: Response) => {
+app.delete("/delete", (req: Request, res: Response) => {
   res.send("This is a DELETE request.");
 });
 
